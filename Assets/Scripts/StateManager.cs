031dbccc30ffb7b0215ec81ebbc91ed463602684
eduard-hutu <eduard.hutu@student.usv.ro>
2019-12-03 16:07:56 +0200

    Move forward
    I made the move forward script

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 namespace SA
 {
-    public class StateManager : MonoBehaviour
+    public class  StateManager : MonoBehaviour
     {
         public float health;
         
         public State currentState;
 
-
+        public MovmentVariables movmentVariables;
         [HideInInspector]
         public float delta;
         [HideInInspector]
         public Transform mTransform;
-
+        [HideInInspector]
+        public new Rigidbody rigidbody; 
+        
         private void Start()
         {
             mTransform = this.transform;
+            rigidbody = GetComponent<Rigidbody>();
         }
+        private void FixedUpdate()
+        {
+            delta = Time.fixedDeltaTime;
+            if (currentState != null)
+            {
+                currentState.FixedTick(this);
+            }
 
+        }
         private void Update()
         {
+            delta = Time.deltaTime;
             if(currentState != null)
             {
                 currentState.Tick(this);
