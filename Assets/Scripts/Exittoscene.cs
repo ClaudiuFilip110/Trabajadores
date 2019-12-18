53fbb348826e775e7cc837344e69569965c849ba
StefanVieriu <vieriucristianstefan1997>
2019-12-18 10:02:31 +0200

    Menu
    Pentru realizarea meniului. S-au efectuat:
    Canvas
    -Meniu
    -Play
    -Quit
    -Rotate caracter script
    -Design butoane.

---
+++ Assets/Scripts/Exittoscene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Exittoscene : MonoBehaviour
+{
+    public void BacktoMeniu(string backmenu)
+
+    {
+        
+        SceneManager.LoadScene(backmenu);
+
+    }
+}
