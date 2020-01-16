2862a448818bd478d2b478408b00a74386ab1fab
StefanVieriu <vieriucristianstefan1997>
2020-01-16 11:22:38 +0200

    Solved visual problemes InGameMeniu
    Atasat butoane noi si functionalitatea lor in dezvoltare

---
+++ Assets/ChangeMusicVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeMusicVolume : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    public Slider Voulme;
+    public AudioSource myMusic;
+    void Update()
+    {
+
+    }
+
+    
+
+    // Update is called once per frame
+    
+}
