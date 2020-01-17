d673a992967311bc8877a1435e9eecf165b551e3
StefanVieriu <vieriucristianstefan1997>
2020-01-17 08:54:53 +0200

    Restructurarea proiectului
    Mutarea scenelor intr-una singura pentru a putea lucra mai usor prin canvas-uri si variabile salvate

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,17 +7,25 @@ public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public static bool GameIsPause = false;
+    public GameObject pauseMenuCredits;
+    public GameObject Caracter;
 
-    public GameObject pauseMenuUI; 
+    public GameObject pauseMenu;
+    public GameObject pauseCOIN;
+    public GameObject pauseMenuUI;
+    public GameObject pauseMenuUI2;
     // Update is called once per frame
     void Update()
+    
     {
-     if(Input.GetKeyDown(KeyCode.Escape))
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
             if(GameIsPause)
-            {
+            {   
                 Resume();
-
-            }else
+              
+            }
+            else
             {
                 Pause();
             }
@@ -26,18 +34,91 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        pauseCOIN.SetActive(true);
         Time.timeScale = 1f;
         GameIsPause = false;
     }
     void Pause()
     {
+        pauseCOIN.SetActive(false);
+
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPause = true;
     }
-    public  void LoadingMenua()
+    public void Play()
+    {
+        pauseCOIN.SetActive(true);
+        pauseMenu.SetActive(false);
+       
+        
+    }
+
+
+    public void SetSettings()
+    {
+        pauseMenuUI.SetActive(false);
+        pauseMenuUI2.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+    public void SetfromMenuSettings()
+    {
+        pauseMenu.SetActive(false);
+        pauseMenuUI2.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+    public void BackSettings()
+    {
+        pauseMenuUI2.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+    public void BackToPlay()
+    {
+        pauseMenu.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    public void CreditsTime()
+    {
+        pauseMenu.SetActive(false);
+        pauseMenuCredits.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPause = true;
+    }
+    public void backCreditsTime()
+    {
+        pauseMenu.SetActive(true);
+        pauseMenuCredits.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+        public  void LoadingMenua()
     {
         SceneManager.LoadScene("Meniu");
+
+    }
+    public void FullScreen()
+    {
+       
+            // Toggle fullscreen
+            Screen.fullScreen = !Screen.fullScreen;
+        Screen.SetResolution(1920, 1080, false);
+    }
+    public void Windowed()
+    {
+              
+            // Switch to 800 x 600 windowed
+            Screen.SetResolution(1400, 1050, false);
+        
+    }
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
     }
     public void Quitgame()
     {
