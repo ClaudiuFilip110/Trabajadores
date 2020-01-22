fac6b69d71139f85f710f5386585e767bbb253ce
ClaudiuFilip110 <clawdelu.filip@gmail.com>
2020-01-22 11:00:16 +0200

    Added Letters in front of Buildings
    added A, B, C, D, E, F, G
    added a RotateText script
    put more folders so it looks better

---
+++ Assets/Scripts/CollectorScripts/Collector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour
+{
+    
+    public  GameObject panel;
+   
+    void OnCollisionEnter(Collision collision)
+    {  
+        panel.gameObject.SetActive(false);
+        
+        if (collision.collider.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            ScoreTextScript.coinAmount += 1;
+            panel.gameObject.SetActive(true);
+
+        }  
+    }
+}
---
+++ Assets/Scripts/Rotate/RotateText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateText : MonoBehaviour
+{
+
+    public float rotationRate = 100f;
+    // degrees per second to rotate in each axis. Set in inspector.
+    public Vector3 rotation;
+    void Start()
+    {
+        rotation = new Vector3(0f, 0f, 1f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation, rotationRate * Time.deltaTime);
+    }
+}
